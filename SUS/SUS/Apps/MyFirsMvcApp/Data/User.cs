96147234966e8e3e002ae7f3203cf6b35df22e2e
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2022-08-21 16:14:09 +0300

    integrating DB in Data

---
+++ SUS/SUS/Apps/MyFirsMvcApp/Data/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCards.Data
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.UserCards = new HashSet<UserCard>();
+        }
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public virtual ICollection<UserCard> UserCards { get; set; }
+    }
+}
---
+++ SUS/SUS/Apps/MyFirsMvcApp/Data/UserCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCards.Data
+{
+    public class UserCard
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public int CardId { get; set; }
+
+        public virtual Card Card { get; set; }
+    }
+}
