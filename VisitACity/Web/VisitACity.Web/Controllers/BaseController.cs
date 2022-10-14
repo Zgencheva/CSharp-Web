7edf71fb1a90592661c6269014f87b226b9f67ca
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2022-10-14 15:17:26 +0300

    addFirstNameClaim

--- VisitACity/Web/VisitACity.Web/Controllers/BaseController.cs
+++ VisitACity/Web/VisitACity.Web/Controllers/BaseController.cs
@@ -2,9 +2,31 @@
 {
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System.Linq;
 
     [Authorize]
     public class BaseController : Controller
     {
+        public string UserFirstName
+        {
+            get
+            {
+                string firstName = string.Empty;
+                if (this.User != null && this.User.HasClaim(c => c.Type == "firstName"))
+                {
+                    firstName = this.User.Claims
+                        .FirstOrDefault(c => c.Type == "firstName").Value;
+                }
+
+                return firstName;
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            this.ViewBag.UserFirstName = this.UserFirstName;
+            base.OnActionExecuted(context);
+        }
     }
 }
