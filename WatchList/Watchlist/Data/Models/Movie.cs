6f23cb5bd5a81bb6c755e01fb7b7200f150a01e8
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2022-10-17 20:55:56 +0300

    WatchList users implementation

---
+++ WatchList/Watchlist/Data/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Watchlist.Data.Models
+{
+    public class Movie
+    {
+        public Movie()
+        {
+            this.UsersMovies = new HashSet<UserMovie>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Director { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        public int? GenreId { get; set; }
+        [ForeignKey(nameof(GenreId))]
+        public Genre? Genre { get; set; }
+
+        public virtual ICollection<UserMovie> UsersMovies { get; set; }
+    }
+}
---
+++ WatchList/Watchlist/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Watchlist.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+            this.UsesrMovies = new HashSet<UserMovie>();
+        }
+        public ICollection<UserMovie> UsesrMovies { get; set; }
+    }
+}
