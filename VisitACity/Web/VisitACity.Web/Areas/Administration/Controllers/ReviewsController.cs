6ee2f50b3b20e4c12c9c8f103a34c3289498e184
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2022-11-05 00:23:41 +0200

    email sender first try

--- VisitACity/Web/VisitACity.Web/Areas/Administration/Controllers/ReviewsController.cs
+++ VisitACity/Web/VisitACity.Web/Areas/Administration/Controllers/ReviewsController.cs
@@ -17,15 +17,7 @@
         public async Task<IActionResult> Delete(int id)
         {
             await this.reviewService.DeleteAsync(id);
-            this.TempData["Message"] = $"Comment deleted successfully.";
             return this.Ok();
         }
-
-        //public async Task<IActionResult> Delete(int id, int attractionId)
-        //{
-        //    await this.reviewService.DeleteAsync(id);
-        //    this.TempData["Message"] = $"Comment deleted successfully.";
-        //    return this.RedirectToAction("Details", "Attractions", new { area = "", id = attractionId });
-        //}
     }
 }
