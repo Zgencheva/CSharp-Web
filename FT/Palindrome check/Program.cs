d514a0d36391b715a25932d356fb7aa39bdc0b1b
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2023-05-30 15:02:33 +0300

    Palindrome check

---
+++ FT/Palindrome check/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Palindrome_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(input));
+        }
+
+        private static bool IsPalindrome(string? input)
+        {
+            char[] inputAsArray = input.ToCharArray();
+            Array.Reverse(inputAsArray);
+            var result = new string(inputAsArray);
+            if (result == input)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
